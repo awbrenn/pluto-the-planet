f95e3a73e53a6f20a90d2e86a16a0f382c4626dc
Austin W Brennan <AWBRENN@clemson.edu>
2016-02-09 16:35:51 -0500

    Joining Erik's and Adam's work to project.

---
+++ Assets/Scripts/eatOnContact.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eatOnContact : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		sizeScale contactee = gameObject.GetComponent<sizeScale>();
+		sizeScale contactor = other.gameObject.GetComponent<sizeScale>();
+
+		float selfSize = contactee.getSize ();
+		float otherSize = contactor.getSize ();
+
+
+		if (other.tag == "Player") 
+		{
+			float sizeDifference = otherSize / selfSize;
+			if (sizeDifference >= 3.5)
+			{
+				Destroy (gameObject);
+				contactor.addSize(selfSize);
+			}
+
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/sizeScale.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sizeScale : MonoBehaviour 
+{
+	public float startingSize = 100f;
+	public float baseSize = 100f;
+	public float sizeChangeOverTimeScale = 1f;
+	
+	private float size;
+	private float setScale;
+	private Vector3 scaleHolder;
+	private float newSize;
+	private float sC;
+
+	// Use this for initialization
+	void Start () {
+		size = startingSize;
+		setScale = size / baseSize;
+		scaleHolder = new Vector3 (setScale, setScale, setScale);
+		transform.localScale = scaleHolder;
+		newSize = startingSize;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (size < newSize) {
+
+			size += Time.deltaTime * sC;
+			if (size > newSize) 
+				{
+				size = newSize;
+				changeSize ();
+				}
+			changeSize ();
+		}
+		if (size > newSize) {
+			
+			size -= Time.deltaTime * sC;
+			if (size > newSize) 
+			{
+				size = newSize;
+				changeSize ();
+			}
+			changeSize ();
+		}
+
+	}
+
+	public float getSize () {
+		return size;
+	}
+
+	public void changeSize() {
+		setScale = size / baseSize;
+		scaleHolder = new Vector3 (setScale, setScale, setScale);
+		transform.localScale = scaleHolder;
+
+	}
+
+	public void addSize (float sizeChange) {
+		newSize += sizeChange;
+		sC = newSize - size;
+		sC = sC * sizeChangeOverTimeScale;
+	}
+}
