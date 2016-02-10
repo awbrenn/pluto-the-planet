78097cec8168c2f360625c56d3910b6c23d37e26
awbrenn <awbrenn@g.clemson.edu>
2016-02-10 14:04:21 -0500

    Setting velocity of projectile based on how fast the user swipes.

--- Assets/Scripts/basicAttackScript.cs
+++ Assets/Scripts/basicAttackScript.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class basicAttackScript : MonoBehaviour {
 	public Camera main_camera;
-	public float speed;
+	public float max_speed = (float) 10.0;
+	private float speed;
 	private bool pluto_pressed = false;
 	private Vector3 start_position;
 	private Vector3 end_position;
 	private GameObject projectile_prefab;
 	private SphereCollider pluto_collider;
+	private float time_pluto_was_pressed;
 
 	// Use this for initialization
 	void Start () {
@@ -24,17 +26,30 @@ public class basicAttackScript : MonoBehaviour {
 			if (Physics.Raycast (ray) && !pluto_pressed) {
 				start_position = Input.mousePosition;
 				pluto_pressed = true;
+				time_pluto_was_pressed = Time.time;
 			}
 		}
 
 		if (Input.GetMouseButtonUp (0) && pluto_pressed) {
 			Vector3 projectile_initial_position;
 			SphereCollider projectile_collider;
+			float delta_time = Time.time - time_pluto_was_pressed;
+			float speed_scale = 100.0f;
 
 			end_position = Input.mousePosition;
-			Vector3 projectile_trajectory = (end_position - start_position).normalized * speed;
+			if (end_position == start_position) {
+				return;
+			}
+
+			Vector3 projectile_trajectory = (end_position - start_position).normalized;
 			projectile_trajectory.z = projectile_trajectory.y;
 			projectile_trajectory.y = 0.0f;
+			speed = (float)((end_position - start_position).magnitude/speed_scale) / delta_time;
+
+			// clamp spped to max speed
+			if (speed >= max_speed) {
+				speed = max_speed;
+			}
 
 			GameObject projectile = Instantiate (projectile_prefab) as GameObject;
 			projectile_collider = projectile.transform.GetComponent<SphereCollider> () as SphereCollider;
