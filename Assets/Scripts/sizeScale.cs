8904739c8ce1cd37bd15f1b93adb709fe5f7ce22
Adam Daniel Wentworth <adwentw@clemson.edu>
2016-02-13 16:19:59 -0500

    got exclusion volume to work, changed size scale to volume

--- Assets/Scripts/sizeScale.cs
+++ Assets/Scripts/sizeScale.cs
@@ -6,7 +6,11 @@ public class sizeScale : MonoBehaviour
 	public float startingSize = 100f;
 	public float baseSize = 100f;
 	public float sizeChangeOverTimeScale = 1f;
-	
+
+	private float volume;
+	private float baseVolume;
+	private float newVolume;
+
 	private float size;
 	private float setScale;
 	private Vector3 scaleHolder;
@@ -15,16 +19,21 @@ public class sizeScale : MonoBehaviour
 
 	// Use this for initialization
 	void Start () {
-		size = startingSize;
-		setScale = size / baseSize;
+		volume = (4f / 3f) * (3.14f) * (startingSize) * (startingSize) * (startingSize);
+		baseVolume= (4f / 3f) * (3.14f) * (baseSize) * (baseSize) * (baseSize);
+
+//		size = startingSize;
+		setScale = volume / baseVolume;
 		scaleHolder = new Vector3 (setScale, setScale, setScale);
 		transform.localScale = scaleHolder;
-		newSize = startingSize;
+		newVolume = volume;
+
+//		newSize = startingSize;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (size < newSize) {
+/*		if (size < newSize) {
 
 			size += Time.deltaTime * sC;
 			if (size > newSize) 
@@ -44,27 +53,49 @@ public class sizeScale : MonoBehaviour
 			}
 			changeSize ();
 		}
+*/
+		if (volume < newVolume) {
+
+			volume += Time.deltaTime * sC;
+			if (volume > newVolume) 
+			{
+				volume = newVolume;
+				changeSize ();
+			}
+			changeSize ();
+		}
+		if (volume > newVolume) {
+
+			volume -= Time.deltaTime * sC;
+			if (volume > newVolume) 
+			{
+				volume = newVolume;
+				changeSize ();
+			}
+			changeSize ();
+		}
 
 	}
 
 	public float getSize () {
+		size = volume;
 		return size;
 	}
 
 	public void setSize (float randSize) {
-		size = randSize;
-		startingSize = size;
+		volume = randSize;
+		startingSize = volume;
 	}
 
 	public void changeSize() {
-		setScale = size / baseSize;
+		setScale = volume / baseVolume;
 		scaleHolder = new Vector3 (setScale, setScale, setScale);
 		transform.localScale = scaleHolder;
 	}
 
 	public void addSize (float sizeChange) {
-		newSize += sizeChange;
-		sC = newSize - size;
+		newVolume += sizeChange;
+		sC = newVolume - volume;
 		sC = sC * sizeChangeOverTimeScale;
 	}
 }
