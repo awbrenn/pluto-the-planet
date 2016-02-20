4c0e70187ffe2baf0ed3306ee6cd1f9f1c720594
Austin Brennan <Austin Brennan>
2016-02-20 10:06:57 -0500

    Coloring harmful food red and eatable food blue.

---
+++ Assets/Scripts/colorFood.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colorFood : MonoBehaviour {
+	// ratio of foodHealth/plutoHealth in order to be edible
+	public float percentageOfPlutosHealthForEating = 0.35f;
+
+	private objectHealth plutosHealth;
+	private objectHealth foodsHealth;
+	private Renderer foodRenderer;
+	// Use this for initialization
+	void Start () {
+		// get plutos health script
+		plutosHealth = GameObject.Find ("Pluto").GetComponent<objectHealth> () as objectHealth;
+		foodsHealth = GetComponent<objectHealth> () as objectHealth;
+		foodRenderer = GetComponent<Renderer> ();
+		//GetComponent<Renderer> ().material.color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (foodsHealth.getHealth () / plutosHealth.getHealth () <= percentageOfPlutosHealthForEating) {
+			foodRenderer.material.color = Color.blue;
+		} else {
+			foodRenderer.material.color = Color.red;
+		}
+	}
+}
