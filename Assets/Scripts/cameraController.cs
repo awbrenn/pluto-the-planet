db365fdacb6d669b069a239aeec8f786c242daf1
Adam Wentworth <adwentw@clemson.edu>
2016-02-20 19:02:27 -0500

    camera stuff 2

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -21,15 +21,15 @@ public class cameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-//		float step = speed * Time.deltaTime;
+		float step = speed * Time.deltaTime;
 		playerSize = player.transform.localScale.x;
 		float playerHealth = (float)player.GetComponent<objectHealth> ().getHealth();
 		cameraHeight = ((playerHealth/100) + 5f);
 
 //		cameraHeight = (playerSize + 5f);
 		camLoc = new Vector3(player.transform.position.x, cameraHeight, player.transform.position.z);
-//		transform.position = Vector3.Lerp(transform.position, camLoc, step);
+		transform.position = Vector3.Lerp(transform.position, camLoc, step);
 
-		transform.position = camLoc;
+//		transform.position = camLoc;
 	}
 }
