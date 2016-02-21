75f0e56bf3e50080234266744e3944485ff31d14
Austin Brennan <awbrenn@clemson.edu>
2016-02-21 13:25:50 -0500

    Adding shooting in boss volume.

--- Assets/Scripts/plutoBasicAttack.cs
+++ Assets/Scripts/plutoBasicAttack.cs
@@ -12,11 +12,13 @@ public class plutoBasicAttack : MonoBehaviour {
 	private GameObject projectile_prefab;
 	private float time_pluto_was_pressed;
 	private Camera main_camera;
+	private float bossVolumeRadius;
 
 	// Use this for initialization
 	void Start () {
 		main_camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>() as Camera;
 		projectile_prefab = Resources.Load ("Prefabs/defaultProjectile") as GameObject;
+		bossVolumeRadius = GameObject.Find ("BossVolume").transform.localScale.x/2.0f;
 	}
 
 
@@ -38,8 +40,6 @@ public class plutoBasicAttack : MonoBehaviour {
 		// using scale of Pluto to determine the initial position of projectile 
 		projectile_initial_position = projectile_trajectory * (transform.localScale.x/2.0f + projectile.transform.localScale.x/2.0f) + transform.position;
 		projectile.transform.position = projectile_initial_position;
-
-		//GameObject newProjectile = (GameObject) Instantiate (projectile);
 
 
 		Rigidbody projectile_rigid_body = projectile.GetComponent<Rigidbody> ();
@@ -69,23 +69,32 @@ public class plutoBasicAttack : MonoBehaviour {
 		if (Input.GetMouseButtonUp (0) && pluto_pressed) {
 			float delta_time = Time.time - time_pluto_was_pressed;
 			float speed_scale = 100.0f;
+			Vector3 bossPosition = GameObject.FindWithTag ("Boss").transform.position;
+			Vector3 projectile_trajectory;
+			Vector3 plutoVelocity = GetComponent<Rigidbody> ().velocity;
 
-			// if the player didn't swipe, we don't spawn a projectile
-			end_position = Input.mousePosition;
-			if (end_position == start_position) {
-				pluto_pressed = false;
-				return;
+			if ((transform.position - bossPosition).magnitude < bossVolumeRadius) {
+				projectile_trajectory = (bossPosition - transform.position - plutoVelocity/3.0f).normalized;
+				speed = 20.0f;
 			}
+			else {
+				// if the player didn't swipe, we don't spawn a projectile
+				end_position = Input.mousePosition;
+				if (end_position == start_position) {
+					pluto_pressed = false;
+					return;
+				}
 
-			// calculate the normalized projectile trajectory
-			Vector3 projectile_trajectory = (end_position - start_position).normalized;
+				// calculate the normalized projectile trajectory
+				projectile_trajectory = (end_position - start_position).normalized;
 
-			// transform the trajectory from the x-y plane to the x-z plane
-			projectile_trajectory.z = projectile_trajectory.y;
-			projectile_trajectory.y = 0.0f;
+				// transform the trajectory from the x-y plane to the x-z plane
+				projectile_trajectory.z = projectile_trajectory.y;
+				projectile_trajectory.y = 0.0f;
 
-			// get the speed (*note speed_scale is a fudge factor, may need to refactor)
-			speed = (float)((end_position - start_position).magnitude/speed_scale) / delta_time;
+				// get the speed (*note speed_scale is a fudge factor, may need to refactor)
+				speed = (float)((end_position - start_position).magnitude/speed_scale) / delta_time;
+			}
 
 			spawnProjectile (projectile_trajectory, speed);
 
