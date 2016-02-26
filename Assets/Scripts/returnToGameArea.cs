50bdd34948a7df5bb069bf2d120427076ac2d448
Austin Brennan <awbrenn@clemson.edu>
2016-02-26 12:11:21 -0500

    Adding transition and safe volumes.

--- Assets/Scripts/returnToGameArea.cs
+++ Assets/Scripts/returnToGameArea.cs
@@ -3,20 +3,21 @@ using System.Collections;
 
 public class returnToGameArea : MonoBehaviour {
 	public float accelerationTowardsCenter = 1.0f;
+	public GameObject boarderVolume;
 
-	float growthVolumeRadius;
+	float boarderVolumeRadius;
 	Rigidbody rigidBody;
 	// Use this for initialization
 	void Start () {
 		// get the radius of the growth volume
-		growthVolumeRadius = GameObject.Find ("GrowthVolume").transform.localScale.x / 2.0f;
+		boarderVolumeRadius = boarderVolume.transform.localScale.x / 2.0f;
 		rigidBody = GetComponent<Rigidbody> () as Rigidbody;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		float distanceFromCenter = transform.position.magnitude;
-		if (distanceFromCenter > growthVolumeRadius) {
+		if (distanceFromCenter > boarderVolumeRadius) {
 			Vector3 directionToCenter = (-1.0f) * transform.position.normalized;
 			rigidBody.velocity += directionToCenter * accelerationTowardsCenter;
 		}
