a85ee677be90bb747c7fa68b9f608e567d5883c1
Adam Daniel Wentworth <adwentw@clemson.edu>
2016-02-26 11:20:03 -0500

    added new scene for next week, camera moves

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -7,14 +7,22 @@ public class cameraController : MonoBehaviour {
 	public GameObject boss;
 	public float speed = 1f;
 	public float camHeight = 10f;
+	public float bossHoldTime = 5f;
+	public float sceneHoldTime = 10f;
+	public float extraIntroHeight = 2f;
 
 	private Vector3 playerLoc;
 	private Vector3 camLoc;
+	private Vector3 sceneCamLoc;
+
+	private float introSceneHeight;
 	private float cameraHeight;
 	private float playerSize;
 	private float spd;
 
-	private bool mainGamePlay = true;
+	private bool introBossLook = true;
+	private bool introSceneLook = false;
+	private bool mainGamePlay = false;
 	private bool inBossVolume = false;
 
 	private Quaternion outerLookDir;
@@ -24,16 +32,38 @@ public class cameraController : MonoBehaviour {
 		outerLookDir = Quaternion.Euler(90, 0, 0);
 		spd = speed;
 		cameraHeight = camHeight;
+		camLoc = new Vector3 (boss.transform.position.x, 5f, boss.transform.position.z);
 		playerLoc = new Vector3(player.transform.position.x, cameraHeight, player.transform.position.z);
-		transform.position = playerLoc;
+		transform.position = camLoc;
+
+		GameObject growthVolume = GameObject.FindGameObjectWithTag ("Growth Volume");
+		introSceneHeight = growthVolume.transform.localScale.y / 2 + extraIntroHeight;
+		sceneCamLoc = camLoc;
+		sceneCamLoc.y = introSceneHeight;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (introBossLook){
+			StartCoroutine (introLookTimer (bossHoldTime));
+			introBossLook = false;
+			Debug.Log ("end intro boss look");
+
+		}
+		if (introSceneLook){
+			Debug.Log ("in introSceneLook");
+			float step = (spd * 4) * Time.deltaTime;
+			transform.position = Vector3.Lerp (transform.position, sceneCamLoc, step);
+			if (transform.position.y >= (sceneCamLoc.y - 1f)){
+				Debug.Log ("begin countdown to main game play");
+				StartCoroutine (sceneLookTimer (sceneHoldTime));
+				introSceneLook = false;
+			}
+		}
+
 		if (mainGamePlay){
 			float playerHealth = (float)player.GetComponent<objectHealth> ().getHealth();
 			cameraHeight = ((playerHealth/100) + camHeight);
-
 			inBossVolume = bossVolume.GetComponent<SphereCollider> ().bounds.Contains (player.transform.position);
 
 			if (inBossVolume) {
@@ -68,4 +98,32 @@ public class cameraController : MonoBehaviour {
 			}
 		}
 	}
+
+	IEnumerator introLookTimer (float lookTime){
+		Debug.Log ("lookTimer started at" + Time.time);
+
+		yield return new WaitForSeconds (lookTime);
+
+		Debug.Log ("lookTimer ended at" + Time.time);
+
+		introSceneLook = true;
+
+	}
+
+	IEnumerator sceneLookTimer (float lookTime){
+		Debug.Log ("lookTimer started at" + Time.time);
+
+		yield return new WaitForSeconds (lookTime);
+
+		Debug.Log ("lookTimer ended at" + Time.time);
+
+		mainGamePlay = true;
+
+	}
+
+/*	Vector3 lookCenter(){
+		Vector3 center = (((boss.transform.position - attachedTo.transform.position).normalized) * ((attachedTo.transform.localScale.x/2f) + 1f));
+
+		return center;
+	}*/
 }
