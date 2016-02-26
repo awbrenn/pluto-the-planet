e83ba32fd3d7730bccbd80da63f2bb77e463317c
Austin Brennan <awbrenn@clemson.edu>
2016-02-26 13:00:19 -0500

    Adding inner boarder volume.

--- Assets/Scripts/returnToGameArea.cs
+++ Assets/Scripts/returnToGameArea.cs
@@ -2,26 +2,42 @@
 using System.Collections;
 
 public class returnToGameArea : MonoBehaviour {
-	public float accelerationTowardsCenter = 1.0f;
-	public GameObject boarderVolumePrefab;
+	public float acceleration = 1.0f;
+	public GameObject innerBoarderVolumePrefab;
+	public GameObject outerBoarderVolumePrefab;
 
-	private float boarderVolumeRadius;
+	private float outerBoarderVolumeRadius;
+	private float innerBoarderVolumeRadius;
 	private Rigidbody rigidBody;
-	private GameObject boarderVolume;
+	private GameObject outerBoarderVolume;
+	private GameObject innerBoarderVolume;
 	// Use this for initialization
 	void Start () {
 		// get the radius of the growth volume
-		boarderVolume = GameObject.FindGameObjectWithTag(boarderVolumePrefab.tag);
-		boarderVolumeRadius = boarderVolume.transform.localScale.x / 2.0f;
+		if (outerBoarderVolumePrefab != null) {
+			outerBoarderVolume = GameObject.FindGameObjectWithTag(outerBoarderVolumePrefab.tag);
+			outerBoarderVolumeRadius = outerBoarderVolume.transform.localScale.x / 2.0f;
+		}
+			
+		if (innerBoarderVolumePrefab != null) {
+			innerBoarderVolume = GameObject.FindGameObjectWithTag(innerBoarderVolumePrefab.tag);
+			innerBoarderVolumeRadius = innerBoarderVolume.transform.localScale.x / 2.0f;
+		}
+
 		rigidBody = GetComponent<Rigidbody> () as Rigidbody;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		float distanceFromCenter = transform.position.magnitude;
-		if (distanceFromCenter > boarderVolumeRadius) {
+		if (distanceFromCenter > outerBoarderVolumeRadius && outerBoarderVolumePrefab != null) {
 			Vector3 directionToCenter = (-1.0f) * transform.position.normalized;
-			rigidBody.velocity += directionToCenter * accelerationTowardsCenter;
+			rigidBody.velocity += directionToCenter * acceleration;
+		}
+
+		if (distanceFromCenter < innerBoarderVolumeRadius && innerBoarderVolumePrefab != null) {
+			Vector3 directionToCenter = transform.position.normalized;
+			rigidBody.velocity += directionToCenter * acceleration;
 		}
 	}
 }
