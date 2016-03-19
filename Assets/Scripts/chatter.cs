fba208a4272471832acae459c0f89111b7a566c3
Austin Brennan <awbrenn@clemson.edu>
2016-03-19 09:44:23 -0400

    WIP - Working on messaging system.

--- Assets/Scripts/chatter.cs
+++ Assets/Scripts/chatter.cs
@@ -4,30 +4,41 @@ using System.Collections.Generic;
 
 public class chatter : MonoBehaviour {
 	public GUISkin chatterSkin;
-	public Vector2 thumbSize;
 	public Vector2 textureOffset = new Vector2(8, 8);
-	public int Spacing = 4;
-	public Vector2 boxSize;
-	private int maxTextMessagesOnScreen = 3;
+	public float spacingPercentage = 0.004f;
+	public float textBoxWidthPercentage = 0.33f;
+	public float textBoxHeightPercentage = 0.33f;
+	public int maxTextMessagesOnScreen = 3;
 	public GameObject[] startTextMessages;
 
+	private int spacing;
 	private Queue<GameObject> queuedTextMessages;
 	private Queue<GameObject> currentTextMessages;
 	private Vector2 GroupPos;
+	public Vector2 thumbSize;
 	private Matrix4x4 GUIScaleMatrix;
 	private int GUIScreenWidth;
 	private int GUIScreenHeight;
+	private Vector2 textBoxSize;
 
 	// Use this for initialization
 	void Start () {
 		GUIScreenWidth = Screen.width;
 		GUIScreenHeight = Screen.height;
 
+		textBoxSize.x = Screen.width * textBoxWidthPercentage;
+		textBoxSize.y = Screen.height * textBoxHeightPercentage;
+
+		thumbSize.x = textBoxSize.y;
+		thumbSize.y = textBoxSize.y;
+
+		spacing = Screen.width * spacing;
+
+		GroupPos.x = Screen.width - textBoxSize.x;
+		GroupPos.y = Screen.height - textBoxSize.y;
+
 		queuedTextMessages = new Queue<GameObject>();
 		currentTextMessages = new Queue<GameObject>();
-
-		GroupPos.x = Screen.width - (boxSize.x);
-		GroupPos.y = Screen.height - (boxSize.y);
 	}
 
 	void queueTextMessage(GameObject textMessage) {
@@ -36,7 +47,7 @@ public class chatter : MonoBehaviour {
 	
 	void FixedUpdate () {
 		// don't add a text message if the max is reached
-		if (currentTextMessages.Count > maxTextMessagesOnScreen) {
+		if (currentTextMessages.Count >= maxTextMessagesOnScreen) {
 			return;
 		}
 
@@ -48,7 +59,7 @@ public class chatter : MonoBehaviour {
 		GUIScaleMatrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(1.0f * Screen.width / GUIScreenWidth, 1.0f * Screen.height / GUIScreenHeight, 1.0F));
 		GUI.matrix = GUIScaleMatrix;
 
-		GUI.skin.box.padding.left = (int)(thumbSize.x + Spacing + 4);
+		GUI.skin.box.padding.left = (int)(thumbSize.x + spacing + 4);
 		Rect TextureBox = new Rect(0, 0, thumbSize.x, thumbSize.y);
 		TextureBox.x += textureOffset.x;
 		TextureBox.y += textureOffset.y;
@@ -57,10 +68,10 @@ public class chatter : MonoBehaviour {
 
 		int i = 0;
 		foreach (GameObject textMessage in currentTextMessages) {
-			int y = Screen.height - ((int)(boxSize.y + (i*boxSize.y)));
-			Rect Group = new Rect(GroupPos.x, GroupPos.y, boxSize.x, boxSize.y);
+			int y = (int)(i*textBoxSize.y);
+			Rect Group = new Rect(GroupPos.x, GroupPos.y - y, textBoxSize.x, textBoxSize.y);
 			GUI.BeginGroup (Group);
-			GUI.Box (new Rect (0, 0, boxSize.x, boxSize.y), textMessage.GetComponent<message> ().text);
+			GUI.Box (new Rect (0, 0, textBoxSize.x, textBoxSize.y), textMessage.GetComponent<message> ().text + " " + i);
 			GUI.Box (new Rect (0, 0, thumbSize.x, thumbSize.y), "");
 			GUI.DrawTexture (TextureBox, textMessage.GetComponent<message> ().characterThumb);
 			GUI.EndGroup ();
