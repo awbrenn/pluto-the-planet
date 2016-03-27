6516a0711b98ae6cac2c396835099c35677e7343
Austin Brennan <Austin Brennan>
2016-03-27 13:23:24 -0400

    Adding gas scarf for venus. Toggling visibility of FX when inside transition volume. Fixing food spawn explosion.

---
+++ Assets/Scripts/destroyFoodInsideOtherFood.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyFoodInsideOtherFood : MonoBehaviour {
+	void OnTriggerEnter (Collider other){
+		// Hack to make sure that food that spawns inside other food is destroyed
+		if (other.CompareTag ("food")  && Time.time < 1.0f) {
+			Destroy (other.gameObject);
+		}
+	}
+}
