550e1614f5ebb5a41f551c33fb5963802d36a39f
Austin Brennan <Austin Brennan>
2016-03-27 14:49:55 -0400

    Adding start menu

--- Assets/Scripts/destroyFoodInsideOtherFood.cs
+++ Assets/Scripts/destroyFoodInsideOtherFood.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class destroyFoodInsideOtherFood : MonoBehaviour {
 	void OnTriggerEnter (Collider other){
+		float currTime = Time.time;
 		// Hack to make sure that food that spawns inside other food is destroyed
-		if (other.CompareTag ("food")  && Time.time < 1.0f) {
+		if (other.CompareTag ("food")  && Time.timeSinceLevelLoad < 1.0f) {
 			Destroy (other.gameObject);
 		}
 	}
---
+++ Assets/Scripts/menuScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+
+	public Canvas menu;
+
+	public Button replay;
+	public Button goToMainMenu;
+	public Button start;
+
+	// Use this for initialization
+	void Start () {
+		menu = menu.GetComponent<Canvas> ();
+		if (replay != null) { replay = replay.GetComponent<Button> (); }
+		if (goToMainMenu != null) { goToMainMenu = goToMainMenu.GetComponent<Button> (); }
+		if (start != null) { start = start.GetComponent<Button> (); }
+	}
+
+	public void replayPressed () {
+		UnityEngine.SceneManagement.SceneManager.LoadScene (1);
+	}
+
+	public void startPressed () {
+		UnityEngine.SceneManagement.SceneManager.LoadScene (1);
+	}
+
+	public void goToMenuPressed () {
+		UnityEngine.SceneManagement.SceneManager.LoadScene (0);
+	}
+}
