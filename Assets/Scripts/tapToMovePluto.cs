61b0226a49f1ff936a5f1d9946ae3ac97f5717c3
Austin W Brennan <awbrenn@po.fx.clemson.edu>
2016-04-01 12:08:17 -0400

    Changing tapToMovePluto

--- Assets/Scripts/tapToMovePluto.cs
+++ Assets/Scripts/tapToMovePluto.cs
@@ -9,6 +9,7 @@ public class tapToMovePluto : MonoBehaviour {
 	public float reverse_acceleration = 4.0f;
 	public float start_max_speed = 20.0f;
 	public float speed_increase_decay_rate = -15.0f;
+	public float sensitivity = 200.0f;
 
 
 	// private member variables
@@ -18,7 +19,7 @@ public class tapToMovePluto : MonoBehaviour {
 	private float max_speed_scaled;
 	private float acceleration_scaled;
 	private Vector3 start_mouse_position;
-
+	private float safe_volume_radius;
 	private bool canMove = true;
 
 	// Use this for initialization
@@ -29,6 +30,7 @@ public class tapToMovePluto : MonoBehaviour {
 		// kill rigid body rotations.
 		pluto_rigid_body = GetComponent<Rigidbody> ();
 		pluto_rigid_body.freezeRotation = true;
+		safe_volume_radius = GameObject.FindGameObjectWithTag ("Safe Volume").transform.localScale.x / 2.0f;
 	}
 
 
@@ -50,26 +52,20 @@ public class tapToMovePluto : MonoBehaviour {
 	void updateSpeed()
 	{
 		max_speed_scaled = start_max_speed * Mathf.Exp(-gameObject.transform.localScale.x / speed_increase_decay_rate);
-		acceleration_scaled = acceleration * Mathf.Exp(-gameObject.transform.localScale.x / speed_increase_decay_rate);
 	}
 
 	void acceleratePlutoInTrajectoryDirection (Vector3 trajectory) 
 	{
 		updateSpeed ();
-		
-		Vector3 pluto_direction = pluto_rigid_body.velocity.normalized;
 
-		if (Vector3.Angle (pluto_direction, trajectory) > 100.0f) {
-			// clamp Pluto's velocity to pluto max speed
-			if ((pluto_rigid_body.velocity + trajectory*acceleration_scaled).magnitude <= max_speed_scaled)
-				pluto_rigid_body.velocity += trajectory*acceleration_scaled*reverse_acceleration;
-		}
-		else {
-			// clamp Pluto's velocity to pluto max speed
-			if ((pluto_rigid_body.velocity + trajectory*acceleration_scaled).magnitude <= max_speed_scaled)
-				pluto_rigid_body.velocity += trajectory*acceleration_scaled;
-	
-		}
+		Vector3 new_velocity;
+
+		new_velocity = trajectory * max_speed_scaled;
+
+		if (new_velocity.magnitude > max_speed_scaled)
+			new_velocity = trajectory.normalized * max_speed_scaled;
+
+		pluto_rigid_body.velocity = new_velocity;
 	}
 
 	public void setCanMove (bool newState){
@@ -80,11 +76,28 @@ public class tapToMovePluto : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		if (canMove && Input.GetMouseButton (0)) {
-			if (!pluto_basic_attack.shooting) {
-				Vector3 trajectory = getNormalizedTrajectory ();
+			Vector3 current_mouse_position;
 
-				acceleratePlutoInTrajectoryDirection (trajectory);
+			// on tap
+			if (Input.GetMouseButtonDown (0)) {
+				start_mouse_position = Input.mousePosition;
 			}
+
+			// on holding down
+			if (Input.GetMouseButton (0)) {
+				current_mouse_position = Input.mousePosition;
+
+				Vector3 trajectory = (current_mouse_position - start_mouse_position) / sensitivity;
+
+				trajectory.z = trajectory.y;
+				trajectory.y = 0.0f;
+
+				Debug.Log (start_mouse_position + "    " + current_mouse_position + "     " + trajectory);
+
+				if (!pluto_basic_attack.shooting && transform.position.magnitude <= safe_volume_radius)
+					acceleratePlutoInTrajectoryDirection (trajectory);
+			}
+
 		}
 		else if (!canMove) {
 			if (pluto_rigid_body.velocity.magnitude > 0) {
