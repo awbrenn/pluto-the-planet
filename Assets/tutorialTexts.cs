5f110e1b7996bf017d3f7e8194104f45855e2a4b
Austin W Brennan <awbrenn@po.fx.clemson.edu>
2016-04-27 13:39:54 -0400

    Adding messages to tutrial and health bars.

---
+++ Assets/tutorialTexts.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tutorialTexts : MonoBehaviour {
+	public GameObject[] messages;
+
+	private chatter chatterBox;
+	// Use this for initialization
+	void Start () {
+		chatterBox = gameObject.GetComponent<chatter> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (GameObject message in messages) {
+			chatterBox.queueTextMessage (message);
+		}	
+
+		messages = null;
+	}
+}
