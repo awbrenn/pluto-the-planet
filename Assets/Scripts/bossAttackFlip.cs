2450dbc61fe4c6e647f5c404b5d23550b9ba5d12
Adam Daniel Wentworth <adwentw@tigress.fx.clemson.edu>
2016-04-27 16:33:50 -0400

    added one time venus chatter

--- Assets/Scripts/bossAttackFlip.cs
+++ Assets/Scripts/bossAttackFlip.cs
@@ -2,9 +2,14 @@
 using System.Collections;
 
 public class bossAttackFlip : MonoBehaviour {
+	public GameObject[] greetings;
+
 	private gameController gameCont;
 	private cameraController camControl;
 
+	private bool oneGreeting = false;
+	private chatter chatterBox;
+	private objectHealth objectHealth;
 
 	// Use this for initialization
 	void Start () {
@@ -12,6 +17,10 @@ public class bossAttackFlip : MonoBehaviour {
 		if (gCHolder != null) gameCont = gCHolder.GetComponent<gameController> () as gameController;
 		GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
 		camControl = camera.GetComponent<cameraController> () as cameraController;
+
+		chatterBox = GameObject.FindGameObjectWithTag ("chatterBox").GetComponent<chatter> ();
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		objectHealth = player.GetComponent <objectHealth> ();
 	}
 	
 	// Update is called once per frame
@@ -22,6 +31,16 @@ public class bossAttackFlip : MonoBehaviour {
 	void OnTriggerEnter(Collider player){
 //		Debug.Log (player.gameObject.tag + " entered Boss V");
 		if (player.gameObject.tag == "Player"){
+			int pHealth = objectHealth.getHealth ();
+			if (!oneGreeting && pHealth >= 450) {
+				oneGreeting = true;
+				if (greetings != null) {
+					foreach (GameObject message in greetings) {
+						chatterBox.queueTextMessage (message);
+					}
+				}
+			}
+
 			if (gameCont != null) {
 				gameCont.activateBoss (true);
 				camControl.setBossLook (true);
--- Assets/Scripts/transitionCameraFlip.cs
+++ Assets/Scripts/transitionCameraFlip.cs
@@ -2,18 +2,35 @@
 using System.Collections;
 
 public class transitionCameraFlip : MonoBehaviour {
+	public GameObject[] messages;
 	private cameraController camControl;
 
+	private bool oneSizeWarning = false;
+	private chatter chatterBox;
+	private objectHealth objectHealth;
 
 	// Use this for initialization
 	void Start () {
 		GameObject camera = GameObject.FindGameObjectWithTag("MainCamera");
 		camControl = camera.GetComponent<cameraController> () as cameraController;
+		chatterBox = GameObject.FindGameObjectWithTag ("chatterBox").GetComponent<chatter> ();
+
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		objectHealth = player.GetComponent <objectHealth> ();
 	}
 
 	void OnTriggerEnter(Collider player){
 //		Debug.Log (player.gameObject.tag + " entered Transition");
 		if (player.gameObject.tag == "Player"){
+			int pHealth = objectHealth.getHealth ();
+			if (!oneSizeWarning && pHealth <= 450) {
+				oneSizeWarning = true;
+				if (messages != null) {
+					foreach (GameObject message in messages) {
+						chatterBox.queueTextMessage (message);
+					}
+				}
+			}
 			if (camControl != null) camControl.setTransitionCamera (true);
 		}
 	}
